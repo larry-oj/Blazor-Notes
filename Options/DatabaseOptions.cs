1ac3dd02047d4a8c4739ef010682b389227373fe
larry-oj <66905431+larry-oj@users.noreply.github.com>
2021-09-15 18:05:32 +0300

    Implementing Configuration pattern + secrets manager

---
+++ Options/DatabaseOptions.cs
@@ -0,0 +1,8 @@
+namespace Blazor_Notes.Options
+{
+    public class DatabaseOptions
+    {
+        public const string Database = "Database";
+        public string ConnectionString { get; set; }
+    }
+}
